67a0e6a0509610020b234207eb0ceb842716a58d
Teodor Dacu <odo@192-168-0-159.rdsnet.ro>
2022-10-04 13:05:44 +0300

    feat: created instructions components

---
+++ src/components/Instructions/Instructions.tsx
@@ -0,0 +1,29 @@
+import style from "./Instructions.module.scss";
+
+const Instructions = () => {
+  return (
+    <div className={style["instructions"]}>
+      <div>
+        <div className={style["instructions__subtitle"]}>Example Input</div>
+        <div className={style["instructions__example"]}>
+          5 3 <br /> <br />
+          1 1 E <br />
+          RFRFRFRF <br /> <br />
+          3 2 N <br />
+          FRRFLLFFRRFLL <br /> <br />
+          0 3 W <br />
+          LLFFFLFLFL <br />
+        </div>
+      </div>
+      <div>
+        <div className={style["instructions__subtitle"]}>Example Output</div>
+        <div className={style["instructions__example"]}>
+          1 1 E <br />
+          3 3 N LOST <br />2 3 S
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Instructions;
